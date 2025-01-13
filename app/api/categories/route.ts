5c10f3c0908b1242b9aadc61bed678ddf3e8695f
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2025-01-13 22:46:02 +0100

    7- Files and folders added!

---
+++ app/api/categories/route.ts
@@ -0,0 +1,13 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+
+export async function POST(request: Request) {
+  try {
+  } catch (error) {}
+}
+
+export async function GET() {
+  try {
+  } catch (error) {}
+}
